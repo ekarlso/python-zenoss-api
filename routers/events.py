b4fef6a2989ba05bf1919170e4b82aa660bc1932
etk <etk@svgvt093.bouvet.local>
2011-01-17 09:09:06 +0100

    Changed default args to None (checked & changed by _build_args in router), added rest of the Commands

--- routers/events.py
+++ routers/events.py
@@ -17,7 +17,46 @@ class Events(RouterBase):
     action = 'EventsRouter'
 
 
-    def query(self, args={}):
+    def query(self, args=None):
         args = self._build_args(args)
+        return self._request(args)
 
+    def queryHistory(self, args=None):
+        args = self._build_args(args)
+        return self._request(args)
+
+    def acknowledge(self, args=None):
+        args = self._build_args(args)
+        return self._request(args)
+
+    def unacknowledge(self, args=None):
+        args = self._build_args(args)
+        return self._request(args)
+
+    def reopen(self, args=None):
+        args = self._build_args(args)
+        return self._request(args)
+
+    def close(self, args=None):
+        args = self._build_args(args)
+        return self._request(args)
+
+    def detail(self, args=None):
+        args = self._build_args(args)
+        return self._request(args)
+
+    def write_log(self, args=None):
+        args = self._build_args(args)
+        return self._request(args)
+
+    def classify(self, args=None):
+        args = self._build_args(args)
+        return self._request(args)
+
+    def add_event(self, args=None):
+        args = self._builds_args(args)
+        return self._request(args)
+
+    def column_config(self, args=None):
+        args = self._builds_args(args)
         return self._request(args)
