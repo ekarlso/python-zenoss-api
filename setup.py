2840647b08f6e12510566a490cef1546b880e45c
etk <etk@svgvt093.bouvet.local>
2011-02-01 14:53:18 +0100

    Renamed to python compatible name

--- setup.py
+++ setup.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python
 
-'''
+# Copyright (C) 2011, Endre Karlson
+# All rights reserved
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
 zenoss-api:  Zenoss JSON API
-'''
+"""
 
 import os, sys
 
@@ -15,7 +31,7 @@ from setuptools import setup, find_packages
 install_requires = ["zope.interface>=3.6.1"]
 
 setupOptions = {
-    'name': 'zenoss-api',
+    'name': 'zenoss_api',
     'author': "Endre Karlson, Dave Carmean, Eric Plaster",
     'author_email': "",
     'description': "Zenoss JSON API Python Library",
